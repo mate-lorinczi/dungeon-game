ecfa140842a982494e9f47f1e40f21824bc7f5bd
nicosala90 <nicosala90@gmail.com>
2023-01-18 13:07:57 +0100

    refactor last commit

--- src/main/java/com/codecool/dungeoncrawl/data/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/data/Item.java
@@ -2,22 +2,17 @@ package com.codecool.dungeoncrawl.data;
 
 import com.codecool.dungeoncrawl.data.actors.Player;
 
-public class Item implements  Drawable {
+public abstract class Item implements Drawable {
 
     private Cell cell;
+
     public Item(Cell cell) {
         this.cell = cell;
         this.cell.setItem(this);
     }
 
-    public void itemAction(Player player){
-    };
+    abstract public void itemAction(Player player);
     public Cell getCell() {
         return cell;
-    }
-
-    @Override
-    public String getTileName() {
-        return null;
     }
 }
